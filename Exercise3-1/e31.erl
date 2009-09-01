948fd9d187592e6d3200ca7382cf220f2cab3806
Hynek Vychodil <vychodil.hynek@gmail.com>
2009-09-01 21:36:49 +0200

    Exercise 3-1

---
+++ Exercise3-1/e31.erl
@@ -0,0 +1,10 @@
+-module(e31).
+
+-export([sum/1, sum/2]).
+
+sum(N) -> sum(1, N).
+
+sum(N, M) when is_integer(N), is_integer(M), N =< M -> sum(M, N, M).
+
+sum(M, M, X) -> X;
+sum(M, N, X) -> sum(M, N + 1, X + N).
