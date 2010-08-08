93b3ac5470207a7255320b62678a7e834ad0910a
Hynek Vychodil <vychodil.hynek@gmail.com>
2010-08-08 22:45:24 +0200

    Exercise 3-10 faster variant

--- Exercise3-10/e310b.erl
+++ Exercise3-10/e310b.erl
@@ -1,4 +1,5 @@
--module(e310).
+% More Erlangish (may be Prologish) and accidentaly faster version of e310
+-module(e310b).
 
 -compile(export_all).
 
@@ -6,36 +7,56 @@ to_words(L) -> string:tokens(L, " \t\n\r").
 
 fill(T, W) ->
   F = fill_words(to_words(T), W),
-  string:join([string:join(L, " ") || L <- F], "\n").
+  fill_(F).
 
-fill_words([], _) -> [];
-fill_words([H|T], W) ->
-  {L, R} = fill_line(T, W - length(H), []),
-  [[H|L]|fill_words(R, W)].
+fill_([]) -> [];
+fill_([LL|F]) -> fill_(F, fill_line(LL, [])).
 
-fill_line([], _, L) -> {lists:reverse(L), []};
+fill_([], R) -> R;
+fill_([L|T], R) ->
+  fill_(T, fill_line(L, [$\n|R])).
+
+fill_line([], R) -> R;
+fill_line([W|L], R) -> fill_line_(L, W ++ R).
+
+fill_line_([], R) -> R;
+fill_line_([W|L], R) -> fill_line_(L, W ++ [$\s|R]).
+
+% it will return lines and words on lines in reversed order
+% benefit reveals when result should be build up to result text
+fill_words(T, W) -> fill_words(T, W, []).
+
+fill_words([], _, R) -> R;
+fill_words([H|T], W, A) ->
+  {L, R} = fill_line(T, W - length(H), [H]),
+  fill_words(R, W, [L|A]).
+
+fill_line([], _, L) -> {L, []};
 fill_line([H|T], W, L) when length(H) < W ->
   fill_line(T, W - length(H) - 1, [H|L]);
-fill_line(R, _, L) -> {lists:reverse(L), R}.
+fill_line(R, _, L) -> {L, R}.
 
 justify(T, W) ->
   F = fill_words(to_words(T), W),
   justify_(F, W).
 
 justify_([], _) -> [];
-justify_([L], _) -> string:join(L, " "); % the last line
-justify_([L|R], W) -> justify_line(L, W) ++ [$\n | justify_(R, W)].
+justify_([LL|F], W) -> justify_(F, W, fill_line(LL, [])).
 
-justify_line([Word], _) -> Word; % one word on line
-justify_line(L, W) ->
-  Words = length(L),
-  Spaces = W - lists:sum([length(Word) || Word <- L]),
-  join(L, Words - 1, Spaces, 1, 0).
+justify_([], _, R) -> R;
+justify_([L|T], W, R) ->
+  justify_(T, W, justify_line(L, W, [$\n|R])).
 
-join([Word], _, _, _, _) -> Word; % the last word on line
-join([W|R], D, M, P, T) ->
+justify_line([Word], _, R) -> Word ++ R; % one word on line
+justify_line([Word|T] = L, W, R) ->
+  Words = length(T),
+  Spaces = W - lists:sum([length(X) || X <- L]),
+  join(T, Words, Spaces, 2, Spaces div Words, Word ++ R).
+
+join([], _, _, _, _, R) -> R;
+join([W|L], D, M, P, T, R) ->
   Take = (P * M) div D - T,
-  W ++ string:chars($\s, Take, join(R, D, M, P + 1, T + Take)).
+  join(L, D, M, P + 1, T + Take, W ++ string:chars($\s, Take, R)).
 
 test() ->
   test_fill(),
@@ -64,11 +85,11 @@ through a sequence of transformations." = fill(testing_text(), 40),
 
 test_justify() ->
 "Write a function that will print this in
-a readable form, so that duplicates  are
+a readable  form, so that duplicates are
 removed and  adjacent  numbers  are  put
 into a range. You might like to think of
 doing this  via a  function which  turns
-the earlier list  of occurrences into  a
+the earlier  list of occurrences  into a
 list                                like
 [{1,2},{4,6},{98,98},{100,100},{102,102}]
 through a sequence of transformations." = justify(testing_text(), 40),
